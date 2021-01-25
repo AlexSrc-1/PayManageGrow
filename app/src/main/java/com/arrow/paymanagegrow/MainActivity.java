24a3926236ff63252568056404f1c68a73f7e884
AlexSrc-1 <Alexandr52844@mail.ru>
2021-01-25 15:56:40 +0500

    Initial commit

---
+++ app/src/main/java/com/arrow/paymanagegrow/MainActivity.java
@@ -0,0 +1,19 @@
+package com.arrow.paymanagegrow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.onboarding);
+    }
+}
